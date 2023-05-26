c41948a80f15e5b5a96a4e666c12794513d6dd54
Lina Duangsay <linaduangsay@gmail.com>
2023-05-26 10:24:00 -0500

    deploy time

--- app/src/main/java/com/nashss/se/beefy/lambdas/GetGoalByNameLambda.java
+++ app/src/main/java/com/nashss/se/beefy/lambdas/GetGoalByNameLambda.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 public class GetGoalByNameLambda extends com.nashss.se.beefy.lambda.LambdaActivityRunner<GetGoalByNameRequest, GetGoalByNameResult>
         implements RequestHandler<LambdaRequest<GetGoalByNameRequest>, LambdaResponse> {
     private final Logger log = LogManager.getLogger();
+
     @Override
     public LambdaResponse handleRequest(LambdaRequest<GetGoalByNameRequest> input, Context context) {
         log.info("handleRequest");
