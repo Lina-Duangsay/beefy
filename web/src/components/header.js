940d44cc229bd9ba83c660e79f661b2d7609823f
Lina Duangsay <linaduangsay@gmail.com>
2023-05-27 00:20:37 -0500

    WHY IS THIS HEADER NOT WORKING

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,4 +1,4 @@
-import MusicPlaylistClient from '../api/musicPlaylistClient';
+import BeefyClient from '../api/beefyClient';
 import BindingClass from "../util/bindingClass";
 
 /**
@@ -14,7 +14,7 @@ export default class Header extends BindingClass {
         ];
         this.bindClassMethods(methodsToBind, this);
 
-        this.client = new MusicPlaylistClient();
+        this.client = new BeefyClient();
     }
 
     /**
@@ -35,7 +35,13 @@ export default class Header extends BindingClass {
         const homeButton = document.createElement('a');
         homeButton.classList.add('header_home');
         homeButton.href = 'index.html';
-        homeButton.innerText = 'Playlists';
+
+        const img = document.createElement('img');
+        img.src = 'beef.png';
+        img.alt = 'beefy';
+        img.style.width = '1800px';
+        img.style.height = '450px';
+        homeButton.appendChild(img);
 
         const siteTitle = document.createElement('div');
         siteTitle.classList.add('site-title');
@@ -48,11 +54,36 @@ export default class Header extends BindingClass {
         const userInfo = document.createElement('div');
         userInfo.classList.add('user');
 
-        const childContent = currentUser
-            ? this.createLogoutButton(currentUser)
-            : this.createLoginButton();
+        if (currentUser) {
+            const dashboardButton = document.createElement('a');
+            dashboardButton.classList.add('button');
+            dashboardButton.href = 'employeeDashboard.html';
+            dashboardButton.innerText = 'Employee Dashboard';
 
-        userInfo.appendChild(childContent);
+            const createOrderButton = document.createElement('a');
+            createOrderButton.classList.add('button');
+            createOrderButton.href = 'createOrder.html';
+            createOrderButton.innerText = 'Create Order';
+
+            const logoutButton = this.createLogoutButton(currentUser);
+
+            userInfo.appendChild(dashboardButton);
+            userInfo.appendChild(document.createTextNode('\u00A0')); // add a space between the buttons
+            userInfo.appendChild(createOrderButton);
+            userInfo.appendChild(document.createTextNode('\u00A0')); // add a space between the buttons
+            userInfo.appendChild(logoutButton);
+
+            const style = window.getComputedStyle(logoutButton);
+            const leftOffset = parseInt(style.getPropertyValue('left'), 10);
+            const buttonWidth = parseInt(style.getPropertyValue('width'), 10);
+
+            createOrderButton.style.left = `${leftOffset - buttonWidth - 80}px`; // adjust the left position of the Create Order button
+            dashboardButton.style.left = `${leftOffset - (2 * buttonWidth) - 160}px`; // adjust the left position of the Dashboard button
+            userInfo.style.width = `${leftOffset + (2 * buttonWidth) + 160}px`; // adjust the width of the userInfo container to include the gap and buttons
+        } else {
+            const loginButton = this.createLoginButton();
+            userInfo.appendChild(loginButton);
+        }
 
         return userInfo;
     }
