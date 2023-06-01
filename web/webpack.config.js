179ad98bfc9aed05e22cb7c025c201ff79f14a09
Lina Duangsay <linaduangsay@gmail.com>
2023-06-01 13:44:48 -0500

    i hate javascript and html

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
     viewGoal: path.resolve(__dirname, 'src', 'pages', 'viewGoal.js'),
     searchPlaylists: path.resolve(__dirname, 'src', 'pages', 'searchPlaylists.js'),
     createGoal: path.resolve(__dirname, 'src', 'pages', 'createGoal.js'),
+    home: path.resolve(__dirname, 'src', 'pages', 'home.js')
   },
   output: {
     path: path.resolve(__dirname, 'build', 'assets'),
