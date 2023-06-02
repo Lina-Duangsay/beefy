26e6d428eec1b059a784e8e3054e1eca79ffad19
Lina Duangsay <linaduangsay@gmail.com>
2023-06-02 12:04:17 -0500

    finished adding some tests, will need to add more eventually

--- app/src/test/java/com/nashss/se/beefy/activity/UpdateGoalDescriptionActivityTest.java
+++ app/src/test/java/com/nashss/se/beefy/activity/UpdateGoalDescriptionActivityTest.java
@@ -8,12 +8,17 @@ import com.nashss.se.beefy.activities.results.UpdateGoalAmountResult;
 import com.nashss.se.beefy.activities.results.UpdateGoalDescriptionResult;
 import com.nashss.se.beefy.dynamodb.GoalDao;
 import com.nashss.se.beefy.dynamodb.models.Goal;
+import com.nashss.se.beefy.exceptions.InvalidAmountException;
+import com.nashss.se.beefy.exceptions.InvalidDescriptionException;
+import com.nashss.se.beefy.metrics.MetricsConstants;
 import com.nashss.se.beefy.metrics.MetricsPublisher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -40,17 +45,16 @@ public class UpdateGoalDescriptionActivityTest {
         String userId = "selene";
         String oldDescription = "need to fuel my obsession for V";
         String newDescription = "need to fuel my new obsession for joshua";
-        String name = "K-Pop Albums";
 
         UpdateGoalDescriptionRequest request = UpdateGoalDescriptionRequest.builder()
                 .withUserId(userId)
                 .withGoalId(goalId)
-                .withDescription(oldDescription)
+                .withDescription(newDescription)
                 .build();
 
         Goal initialGoal = new Goal();
         initialGoal.setUserId(userId);
-        initialGoal.setDescription(newDescription);
+        initialGoal.setDescription(oldDescription);
         initialGoal.setGoalId(goalId);
 
         when(goalDao.getGoal(goalId)).thenReturn(initialGoal);
@@ -63,5 +67,34 @@ public class UpdateGoalDescriptionActivityTest {
         assertEquals(goalId, result.getModel().getGoalId());
         assertEquals(userId, result.getModel().getUserId());
         assertEquals(newDescription, result.getModel().getDescription());
+    }
+
+    @Test
+    public void handleRequest_nullDescriptionProvided_throwsInvalidDescriptionException() {
+        // GIVEN
+        String goalId = "1589";
+        String userId = "selene";
+        String badDescription = null;
+
+        UpdateGoalDescriptionRequest request = UpdateGoalDescriptionRequest.builder()
+                .withUserId(userId)
+                .withGoalId(goalId)
+                .withDescription(badDescription)
+                .build();
+
+        Goal newGoal = new Goal();
+        newGoal.setGoalId(goalId);
+        newGoal.setUserId(userId);
+        newGoal.setDescription(badDescription);
+
+        when(goalDao.getGoal(goalId)).thenReturn(newGoal);
+
+        // WHEN + THEN
+        try {
+            updateGoalDescriptionActivity.handleRequest(request);
+            fail("Expected InvalidDescriptionException to be thrown");
+        } catch (InvalidDescriptionException e) {
+            verify(metricsPublisher).addCount(MetricsConstants.UPDATEGOALDESCRIPTION_DESCRIPTIONINVALID_COUNT, 1);
+        }
     }
 }
