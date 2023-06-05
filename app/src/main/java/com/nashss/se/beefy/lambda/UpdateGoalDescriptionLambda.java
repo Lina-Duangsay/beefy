85e551057e1bf70e718062cd5e7ec1058b302057
Lina Duangsay <linaduangsay@gmail.com>
2023-06-05 12:19:12 -0500

    everything works yay

--- app/src/main/java/com/nashss/se/beefy/lambda/UpdateGoalDescriptionLambda.java
+++ app/src/main/java/com/nashss/se/beefy/lambda/UpdateGoalDescriptionLambda.java
@@ -1,4 +1,26 @@
 package com.nashss.se.beefy.lambda;
 
-public class UpdateGoalDescriptionLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.beefy.activities.requests.UpdateGoalDescriptionRequest;
+import com.nashss.se.beefy.activities.results.UpdateGoalDescriptionResult;
+
+public class UpdateGoalDescriptionLambda extends LambdaActivityRunner<UpdateGoalDescriptionRequest, UpdateGoalDescriptionResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<UpdateGoalDescriptionRequest>, LambdaResponse> {
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateGoalDescriptionRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    UpdateGoalDescriptionRequest unauthenticatedRequest = input.fromBody(UpdateGoalDescriptionRequest.class);
+                    return input.fromUserClaims(claims ->
+                            UpdateGoalDescriptionRequest.builder()
+                                    .withDescription(unauthenticatedRequest.getDescription())
+                                    .withGoalId(unauthenticatedRequest.getGoalId())
+                                    .withUserId(claims.get("email"))
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideUpdateGoalDescriptionActivity().handleRequest(request)
+        );
+    }
 }
