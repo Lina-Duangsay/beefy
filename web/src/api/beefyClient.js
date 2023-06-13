01f23b056b056862a58da3a75c48bca2bf8d9fe8
Lina Duangsay <linaduangsay@gmail.com>
2023-06-13 14:48:27 -0500

    a

--- web/src/api/beefyClient.js
+++ web/src/api/beefyClient.js
@@ -196,30 +196,36 @@ export default class BeefyClient extends BindingClass {
         }
     }
 
-
-
-    /**
-    get all order data for order table
-    */
-
-    async viewAllGoals() {
+    async getUsersGoals(errorCallBack) {
         try {
-            const token = await this.getTokenOrThrow("Only authenticated users can view goals.");
-            const response = await this.axiosClient.get(`/goals/`, {
+            const token = await this.getTokenOrThrow("You must be logged in to view your goals.");
+            const response = await this.axiosClient.get(`goals/getAllGoals/`, {
                 headers: {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json'
+                    Authorization: `Bearer ${token}`
                 }
             });
-            const goals = response.data.goals.map(goal => {
-                const { id: goalId, name, category, goalAmount, description, priority } = goal;
+            return Array.from(response.data);
+        } catch (error) {
+            this.handleError(error, errorCallBack);
+        }
+    }
+
+    /**
+    get all goal data for goal table
+      */
+    async getAllGoalData() {
+        try {
+            const response = await this.axiosClient.get(`/goals/`);
+            const orders = response.data.orders.map(order => {
+                const { id: goalId, name, category, goalAmount, description, priority, completionStatus } = goal;
                 return {
                     goalId,
                     name,
                     category,
                     goalAmount,
                     description,
-                    priority
+                    priority,
+                    completionStatus
                 };
             });
             return goals;
@@ -227,93 +233,6 @@ export default class BeefyClient extends BindingClass {
             this.handleError(error, errorCallback);
         }
     }
-
-
-
-
-
-    // /**
-    //  * Get the songs on a given playlist by the playlist's identifier.
-    //  * @param id Unique identifier for a playlist
-    //  * @param errorCallback (Optional) A function to execute if the call fails.
-    //  * @returns The list of songs on a playlist.
-    //  */
-    // async getPlaylistSongs(id, errorCallback) {
-    //     try {
-    //         const response = await this.axiosClient.get(`playlists/${id}/songs`);
-    //         return response.data.songList;
-    //     } catch (error) {
-    //         this.handleError(error, errorCallback)
-    //     }
-    // }
-
-    // /**
-    //  * Create a new playlist owned by the current user.
-    //  * @param name The name of the playlist to create.
-    //  * @param tags Metadata tags to associate with a playlist.
-    //  * @param errorCallback (Optional) A function to execute if the call fails.
-    //  * @returns The playlist that has been created.
-    //  */
-    // async createPlaylist(name, tags, errorCallback) {
-    //     try {
-    //         const token = await this.getTokenOrThrow("Only authenticated users can create playlists.");
-    //         const response = await this.axiosClient.post(`playlists`, {
-    //             name: name,
-    //             tags: tags
-    //         }, {
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`
-    //             }
-    //         });
-    //         return response.data.playlist;
-    //     } catch (error) {
-    //         this.handleError(error, errorCallback)
-    //     }
-    // }
-
-    // /**
-    //  * Add a song to a playlist.
-    //  * @param id The id of the playlist to add a song to.
-    //  * @param asin The asin that uniquely identifies the album.
-    //  * @param trackNumber The track number of the song on the album.
-    //  * @returns The list of songs on a playlist.
-    //  */
-    // async addSongToPlaylist(id, asin, trackNumber, errorCallback) {
-    //     try {
-    //         const token = await this.getTokenOrThrow("Only authenticated users can add a song to a playlist.");
-    //         const response = await this.axiosClient.post(`playlists/${id}/songs`, {
-    //             id: id,
-    //             asin: asin,
-    //             trackNumber: trackNumber
-    //         }, {
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`
-    //             }
-    //         });
-    //         return response.data.songList;
-    //     } catch (error) {
-    //         this.handleError(error, errorCallback)
-    //     }
-    // }
-
-    // /**
-    //  * Search for a soong.
-    //  * @param criteria A string containing search criteria to pass to the API.
-    //  * @returns The playlists that match the search criteria.
-    //  */
-    // async search(criteria, errorCallback) {
-    //     try {
-    //         const queryParams = new URLSearchParams({ q: criteria })
-    //         const queryString = queryParams.toString();
-
-    //         const response = await this.axiosClient.get(`playlists/search?${queryString}`);
-
-    //         return response.data.playlists;
-    //     } catch (error) {
-    //         this.handleError(error, errorCallback)
-    //     }
-
-    // }
 
 
     /**
